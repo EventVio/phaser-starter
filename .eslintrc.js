5901915c383f0be851a2c158b7915430ba3deeea
MLK <mlk.cerberus@outlook.com>
2022-03-23 20:09:47 -0500

    build(npm): add unused imports and variables

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,21 @@
 module.exports = {
   extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
-  plugins: ['simple-import-sort'],
+  plugins: ['simple-import-sort', 'unused-imports', '@typescript-eslint'],
   rules: {
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'all',
+        argsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
     'no-console': 'warn',
   },
 }
